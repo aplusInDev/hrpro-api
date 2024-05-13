ccbf52f6c01e4b420aab779b6f6622ae0e9148ed
aplusInDev <laabid.abdessamadplus@gmail.com>
2024-05-13 13:48:50 +0100

    POST/ and PUT/ department done

--- api/v1/views/departments.py
+++ api/v1/views/departments.py
@@ -3,6 +3,7 @@
 from flask import jsonify, request, abort
 from api.v1.views import app_views
 from models import storage, Department, Company
+from api.v1.auth.middleware import session_required
 
 
 @app_views.route('companies/<company_id>/departments', methods=['GET'], strict_slashes=False)
@@ -23,33 +24,50 @@ def get_department(department_id):
     return jsonify(department.to_dict())
 
 @app_views.route('companies/<company_id>/departments', methods=['POST'], strict_slashes=False)
-def post_department(company_id):
+@session_required
+def post_department(account, company_id):
     """ post department """
+    if account.role != "admin":
+        return jsonify({"error": "Unauthorized"}), 401
     company = storage.get(Company, company_id)
     if company is None:
         abort(404)
-    data = request.get_json()
-    if data is None:
-        return 'Not a JSON', 400
-    department = Department(**data)
-    department.company_id = company_id
-    department.save()
-    return jsonify(department.to_dict()), 201
+    data = request.form
+    if not data:
+        return jsonify({"error": "unvalid request"}), 400
+
+    from api.v1.utils.validate_field import handle_update_info
+    data = data.to_dict()
+    data = handle_update_info("department", company_id, data)
+    if data:
+        data = str(data)
+        new_department = Department(info=data, company_id=company_id)
+        new_department.save()
+        return jsonify(new_department.to_dict())
+
+    return jsonify({"error": "unvalid request"}), 400
+    
 
 @app_views.route('departments/<department_id>', methods=['PUT'], strict_slashes=False)
-def put_department(department_id):
+@session_required
+def put_department(account, department_id):
     """ put department """
+    if account.role != "admin":
+        abort(401)
     department = storage.get(Department, department_id)
     if department is None:
         abort(404)
-    data = request.get_json()
-    if data is None:
-        return 'Not a JSON', 400
-    for key, value in data.items():
-        if key not in ['id', 'created_at', 'updated_at']:
-            setattr(department, key, value)
-    department.save()
-    return jsonify(department.to_dict())
+    data = request.form
+    if not data:
+        return jsonify({"error": "unvalid request"}), 400
+    from api.v1.utils.validate_field import handle_update_info
+    data = data.to_dict()
+    data = handle_update_info("department", department.company_id, data)
+    if data:
+        department.info = str(data)
+        department.save()
+        return jsonify(department.to_dict())
+    return jsonify({"error": "unvalid request"}), 400
 
 @app_views.route('departments/<department_id>', methods=['DELETE'], strict_slashes=False)
 def delete_department(department_id):
