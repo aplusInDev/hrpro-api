59fe7a94c02e3da8731fe1c1fa02e46a4bb68acb
aplusInDev <laabid.abdessamadplus@gmail.com>
2024-05-17 20:54:10 +0100

    adding sql-dump file

---
+++ 0-sql-dump.sql
@@ -0,0 +1,26 @@
+-- adding new employees
+
+LOCK TABLES `employees` WRITE;
+
+
+INSERT INTO `employees`
+VALUES ('cd736ecb-9aff-400f-a21c-af244ced2111', '2024-05-13 18:38:28', '2024-05-13 18:38:28', 'cd0a3a9e-0f96-48df-b0ef-20907c548cbf', '12947b4d-e495-41b4-8466-932bd4c35fcc', '0828f5b6-2827-42dc-b88e-a43173b68e29', '{"first name": "aplus1", "last name": "test"}'),
+('cd736ecb-9aff-400f-a21c-af244ced2112', '2024-05-13 18:38:28', '2024-05-13 18:38:28', 'cd0a3a9e-0f96-48df-b0ef-20907c548cbf', '12947b4d-e495-41b4-8466-932bd4c35fcc', '0828f5b6-2827-42dc-b88e-a43173b68e29', '{"first name": "aplus2", "last name": "test"}'),
+('cd736ecb-9aff-400f-a21c-af244ced2113', '2024-05-13 18:38:28', '2024-05-13 18:38:28', 'cd0a3a9e-0f96-48df-b0ef-20907c548cbf', '12947b4d-e495-41b4-8466-932bd4c35fcc', '0828f5b6-2827-42dc-b88e-a43173b68e29', '{"first name": "aplus3", "last name": "test"}'),
+('cd736ecb-9aff-400f-a21c-af244ced2114', '2024-05-13 18:38:28', '2024-05-13 18:38:28', 'cd0a3a9e-0f96-48df-b0ef-20907c548cbf', '12947b4d-e495-41b4-8466-932bd4c35fcc', '0828f5b6-2827-42dc-b88e-a43173b68e29', '{"first name": "aplus4", "last name": "test"}'),
+('cd736ecb-9aff-400f-a21c-af244ced2115', '2024-05-13 18:38:28', '2024-05-13 18:38:28', 'cd0a3a9e-0f96-48df-b0ef-20907c548cbf', '12947b4d-e495-41b4-8466-932bd4c35fcc', '0828f5b6-2827-42dc-b88e-a43173b68e29', '{"first name": "aplus5", "last name": "test"}'),
+('cd736ecb-9aff-400f-a21c-af244ced2116', '2024-05-13 18:38:28', '2024-05-13 18:38:28', 'cd0a3a9e-0f96-48df-b0ef-20907c548cbf', '12947b4d-e495-41b4-8466-932bd4c35fcc', '0828f5b6-2827-42dc-b88e-a43173b68e29', '{"first name": "aplus6", "last name": "test"}'),
+('cd736ecb-9aff-400f-a21c-af244ced2117', '2024-05-13 18:38:28', '2024-05-13 18:38:28', 'cd0a3a9e-0f96-48df-b0ef-20907c548cbf', '12947b4d-e495-41b4-8466-932bd4c35fcc', '0828f5b6-2827-42dc-b88e-a43173b68e29', '{"first name": "aplus7", "last name": "test"}'),
+('cd736ecb-9aff-400f-a21c-af244ced2118', '2024-05-13 18:38:28', '2024-05-13 18:38:28', 'cd0a3a9e-0f96-48df-b0ef-20907c548cbf', '12947b4d-e495-41b4-8466-932bd4c35fcc', '0828f5b6-2827-42dc-b88e-a43173b68e29', '{"first name": "aplus8", "last name": "test"}'),
+('cd736ecb-9aff-400f-a21c-af244ced2119', '2024-05-13 18:38:28', '2024-05-13 18:38:28', 'cd0a3a9e-0f96-48df-b0ef-20907c548cbf', '12947b4d-e495-41b4-8466-932bd4c35fcc', '0828f5b6-2827-42dc-b88e-a43173b68e29', '{"first name": "aplus9", "last name": "test"}'),
+('cd736ecb-9aff-400f-a21c-af244ced2120', '2024-05-13 18:38:28', '2024-05-13 18:38:28', 'cd0a3a9e-0f96-48df-b0ef-20907c548cbf', '12947b4d-e495-41b4-8466-932bd4c35fcc', '0828f5b6-2827-42dc-b88e-a43173b68e29', '{"first name": "aplus10", "last name": "test"}'),
+('cd736ecb-9aff-400f-a21c-af244ced2121', '2024-05-13 18:38:28', '2024-05-13 18:38:28', 'cd0a3a9e-0f96-48df-b0ef-20907c548cbf', '12947b4d-e495-41b4-8466-932bd4c35fcc', '0828f5b6-2827-42dc-b88e-a43173b68e29', '{"first name": "aplus11", "last name": "test"}'),
+('cd736ecb-9aff-400f-a21c-af244ced2122', '2024-05-13 18:38:28', '2024-05-13 18:38:28', 'cd0a3a9e-0f96-48df-b0ef-20907c548cbf', '12947b4d-e495-41b4-8466-932bd4c35fcc', '0828f5b6-2827-42dc-b88e-a43173b68e29', '{"first name": "aplus12", "last name": "test"}'),
+('cd736ecb-9aff-400f-a21c-af244ced2123', '2024-05-13 18:38:28', '2024-05-13 18:38:28', 'cd0a3a9e-0f96-48df-b0ef-20907c548cbf', '12947b4d-e495-41b4-8466-932bd4c35fcc', '0828f5b6-2827-42dc-b88e-a43173b68e29', '{"first name": "aplus13", "last name": "test"}'),
+('cd736ecb-9aff-400f-a21c-af244ced2124', '2024-05-13 18:38:28', '2024-05-13 18:38:28', 'cd0a3a9e-0f96-48df-b0ef-20907c548cbf', '12947b4d-e495-41b4-8466-932bd4c35fcc', '0828f5b6-2827-42dc-b88e-a43173b68e29', '{"first name": "aplus14", "last name": "test"}'),
+('cd736ecb-9aff-400f-a21c-af244ced2125', '2024-05-13 18:38:28', '2024-05-13 18:38:28', 'cd0a3a9e-0f96-48df-b0ef-20907c548cbf', '12947b4d-e495-41b4-8466-932bd4c35fcc', '0828f5b6-2827-42dc-b88e-a43173b68e29', '{"first name": "aplus15", "last name": "test"}'),
+('cd736ecb-9aff-400f-a21c-af244ced2126', '2024-05-13 18:38:28', '2024-05-13 18:38:28', 'cd0a3a9e-0f96-48df-b0ef-20907c548cbf', '12947b4d-e495-41b4-8466-932bd4c35fcc', '0828f5b6-2827-42dc-b88e-a43173b68e29', '{"first name": "aplus16", "last name": "test"}'),
+('cd736ecb-9aff-400f-a21c-af244ced2127', '2024-05-13 18:38:28', '2024-05-13 18:38:28', 'cd0a3a9e-0f96-48df-b0ef-20907c548cbf', '12947b4d-e495-41b4-8466-932bd4c35fcc', '0828f5b6-2827-42dc-b88e-a43173b68e29', '{"first name": "aplus17", "last name": "test"}'),
+('cd736ecb-9aff-400f-a21c-af244ced2128', '2024-05-13 18:38:28', '2024-05-13 18:38:28', 'cd0a3a9e-0f96-48df-b0ef-20907c548cbf', '12947b4d-e495-41b4-8466-932bd4c35fcc', '0828f5b6-2827-42dc-b88e-a43173b68e29', '{"first name": "aplus18", "last name": "test"}'),
+('cd736ecb-9aff-400f-a21c-af244ced2129', '2024-05-13 18:38:28', '2024-05-13 18:38:28', 'cd0a3a9e-0f96-48df-b0ef-20907c548cbf', '12947b4d-e495-41b4-8466-932bd4c35fcc', '0828f5b6-2827-42dc-b88e-a43173b68e29', '{"first name": "aplus19", "last name": "test"}'),
+('cd736ecb-9aff-400f-a21c-af244ced2130', '2024-05-13 18:38:28', '2024-05-13 18:38:28', 'cd0a3a9e-0f96-48df-b0ef-20907c548cbf', '12947b4d-e495-41b4-8466-932bd4c35fcc', '0828f5b6-2827-42dc-b88e-a43173b68e29', '{"first name": "aplus20", "last name": "test"}');
