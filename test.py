da50586a5ec655901aa395ad90db551e604a6a55
aplusInDev <laabid.abdessamadplus@gmail.com>
2024-05-19 13:16:02 +0100

    unimportant changes (changing in test.py file)

--- test.py
+++ test.py
@@ -3,10 +3,11 @@
 
 from flask import Flask, jsonify
 import asyncio
-from time import perf_counter
-from sqlalchemy import create_engine, Column, String, Integer
+from time import perf_counter, sleep
+from sqlalchemy import create_engine, Column, String, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -27,38 +28,44 @@ class TestAsync(Base):
     __tablename__ = 'test_async'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(50), nullable=True)
+    created_at = Column(DateTime, default=datetime.now)
 
 @app.route('/async_test', methods=['GET'])
 async def async_test():
     """ async test """
+    start = datetime.now()
     s = perf_counter()
     # await asyncio.gather(insertion_handler())
     # await asyncio.gather(*[insertion_handler() for _ in range(1000)])
     asyncio.create_task(async_handler())
     elapsed = perf_counter() - s
-    return jsonify({"time": f"{elapsed:0.2f} seconds"})
+    end = datetime.now()
+    return jsonify({"time": f"{elapsed:0.2f} seconds", "start": start, "end": end})
 
 async def async_handler():
-    asyncio.sleep(20)
+    # asyncio.sleep(5)
+    sleep(5)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_instance = TestAsync(name="test")
+    new_instance = TestAsync(name="test async")
     session.add(new_instance)
     session.commit()
 
 @app.route('/sync_test', methods=['GET'])
 def sync_test():
     """ async test """
+    start = datetime.now()
     s = perf_counter()
     sync_handler()
     elapsed = perf_counter() - s
-    return jsonify({"time": f"{elapsed:0.2f} seconds"})
+    end = datetime.now()
+    return jsonify({"time": f"{elapsed:0.2f} seconds", "start": start, "end": end})
 
 def sync_handler():
-    asyncio.sleep(20)
+    sleep(5)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_instance = TestAsync(name="test")
+    new_instance = TestAsync(name="test sync")
     session.add(new_instance)
     session.commit()
 
