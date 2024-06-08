db1343fbfc49813550d31eb8bc74967207d08891
aplusInDev <laabid.abdessamadplus@gmail.com>
2024-06-08 16:52:41 +0100

    successfully sending welcome_email asyncronously

--- api/v1/helpers/tasks/mail_tasks.py
+++ api/v1/helpers/tasks/mail_tasks.py
@@ -1,25 +1,27 @@
 from flask_mail import Message
 from os import getenv
 from api.celery_app import app
+from flask import render_template
 
 
 @app.task
 def send_welcome_mail_task(name, email, password):
     """Celery task to send welcome email."""
     from api.v1.app import create_app, mail
-    app = create_app()  # Create an instance of your Flask app
+    app = create_app()  # Create an instance of our Flask app
     with app.app_context():  # Push the application context
+        # we use app_context to have access to the application context
+        # because we are outside the application context
         try:
+            data = {
+                "name": name,
+                "email": email,
+                "password": password,
+                "login_link": "http://localhost:3000/login"
+            }
             msg = Message("Welcome to HRPro", sender=getenv('HRPRO_EMAIL'),
                         recipients=[email])
-            msg.html = f"""
-                <div>
-                    <h1>Welcome {name} to HRPro</h1>
-                    <p>Your account has been created successfully.</p>
-                    <p>Your password is: {password}</p>
-                    <p>Click <a href="http://localhost:3000/login">here</a> to login.</p>
-                </div>
-            """
+            msg.html = render_template("email_welcome.html", data=data)
             mail.send(msg)
             return "task done successfully"
         except Exception as err:
