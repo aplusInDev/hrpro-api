c6b11de3b42187f9ed4a7a4fb35dc0079811ba5d
aplusInDev <laabid.abdessamadplus@gmail.com>
2024-06-09 10:58:34 +0100

    adding comment to celery_app explaining how to start it

--- api/celery_app.py
+++ api/celery_app.py
@@ -1,5 +1,8 @@
 from celery import Celery
 from api.celery_config import config
 
+# to start celery app
+# celery -A api.celery_app worker -l info
+
 app = Celery('celery_app')
 app.config_from_object(config)
