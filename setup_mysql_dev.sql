5fd03bd60dcbd18664f75ac244557a6c7d18c358
aplusInDev <laabid.abdessamadplus@gmail.com>
2024-07-18 13:51:25 +0100

    moving sql scripts to sql folder

--- setup_mysql_dev.sql
+++
@@ -1,8 +0,0 @@
--- setup_mysql_dev.sql.
--- This file is used to create the database and tables for the
--- hrpro project.
-CREATE DATABASE IF NOT EXISTS hrpro_dev_db;
-CREATE USER IF NOT EXISTS 'hrpro_dev'@'localhost' IDENTIFIED BY 'hrpro_dev_pwd';
-GRANT USAGE ON *.* TO 'hrpro_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hrpro_dev'@'localhost';
-GRANT ALL PRIVILEGES ON hrpro_dev_db.* TO 'hrpro_dev'@'localhost';
